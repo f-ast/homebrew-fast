447b8c1d53ab23c7d823a600cf28fea839c05c8d
Yijun Yu <yijun.yu@gmail.com>
2017-08-06 14:35:52 +0800

    0.0.4 => 0.0.5

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -1,11 +1,11 @@
 class Fast < Formula
   desc "Flattening Abstract Syntax Trees"
   homepage "https://github.com/f-ast/fast"
-  url "https://github.com/f-ast/fast/archive/v0.0.4.tar.gz"
+  url "https://github.com/f-ast/fast/archive/v0.0.5.tar.gz"
   sha256 "e22faac6f8552869d31594f226c933baccad471f20420bd1d99e34f384ff840b"
 
   bottle do
-    root_url "https://github.com/f-ast/fast/releases/download/v0.0.4"
+    root_url "https://github.com/f-ast/fast/releases/download/v0.0.5"
     cellar :any
     sha256 "60b693ed48e6d331a1cbafdccb5011bbaf559ce78e16170880a49e54a8515cd0" => :sierra
     sha256 "1e77ab41415d37827738aac32ab28ee756a78befbb9576f0f4b3ef8ca6cac815" => :el_capitan
